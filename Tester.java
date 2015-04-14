88d8caf57eb8dc56b0c4308c2290a0ff40fb2901
Roger Jaffe <rogerjaffe@gmail.com>
2015-04-14 10:41:03 -0700

    Initial commit

---
+++ Tester.java
@@ -0,0 +1,14 @@
+
+/**
+ * Hello World: The first program everyb
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tester {   // We could put any name here besides 
+
+    public static void main(String args[]) {
+
+    }
+
+}
